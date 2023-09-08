beba8f2a2e8fbc75a1df24de146f87f40cb00612
annayagyu <asotto4668@gmail.com>
2023-09-08 17:00:03 -0300

    sexto commit

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -7,7 +7,7 @@ function Sobre(){
             <div>
                 <img src='src/assets/lorem2.png'></img>
                 <p>
-                orem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam quis arcu convallis hendrerit. Maecenas euismod, purus nec malesuada suscipit, justo libero consequat metus, ut efficitur nulla sapien sed elit. Nulla facilisi. Nulla facilisis tortor ut congue lacinia. Fusce vel purus vitae dui iaculis pellentesque. Quisque non lectus in elit egestas tristique. In hac habitasse platea dictumst. Etiam eu efficitur ante, nec varius ex. Sed venenatis bibendum libero, ac euismod lorem pharetra a. Fusce at sapien eu arcu fringilla eleifend eget in ligula. Sed ac lorem quam. In sit amet congue elit.
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam quis arcu convallis hendrerit. Maecenas euismod, purus nec malesuada suscipit, justo libero consequat metus, ut efficitur nulla sapien sed elit. Nulla facilisi. Nulla facilisis tortor ut congue lacinia. Fusce vel purus vitae dui iaculis pellentesque. Quisque non lectus in elit egestas tristique. In hac habitasse platea dictumst. Etiam eu efficitur ante, nec varius ex. Sed venenatis bibendum libero, ac euismod lorem pharetra a. Fusce at sapien eu arcu fringilla eleifend eget in ligula. Sed ac lorem quam. In sit amet congue elit.
                 </p>
             </div>
 
